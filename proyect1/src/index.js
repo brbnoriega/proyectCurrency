989c3663b1def57b41ae006eec675974276294e5
brbnoriega <barbaranoriega330@gmail.com>
2023-01-17 23:36:54 -0300

    changes

--- proyect1/src/index.js
+++ proyect1/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import CurrencyChange from './CurrencyChange';
 import reportWebVitals from './reportWebVitals';
 
